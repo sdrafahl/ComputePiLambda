f0bbe5f60b61932475dd73033e479a41261d5ff0
Shane Drafahl <drafahls@gmail.com>
2019-01-23 18:10:38 -0600

    Wrote code for lambda

---
+++ compute.js
@@ -0,0 +1,14 @@
+exports.compute = () => {
+  let x;
+  let denominator = 1;
+  let pi = 0;
+  for(x =0;x<1000;x++) {
+    if(x % 2 == 0) {
+      pi += 1/denominator;
+    } else {
+      pi -= 1/denominator;
+    }
+    denominator += 2;
+  }
+  return pi * 4;
+}
