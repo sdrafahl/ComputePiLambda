e5a6834d02f3bd661be342cb952ad3611ce6894f
shane <sdrafahl@iastate.edu>
2019-02-01 22:11:09 -0600

    Computed a number even closer to pi

--- lambdaCode/index.js
+++ lambdaCode/index.js
@@ -2,7 +2,7 @@ exports.handler = (event, context, callback) => {
   let x;
   let denominator = 1;
   let pi = 0;
-  for(x =0;x<10000;x++) {
+  for(x =0;x<1000000000;x++) {
     if(x % 2 == 0) {
       pi += 1/denominator;
     } else {
